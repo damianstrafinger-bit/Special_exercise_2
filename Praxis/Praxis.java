1e118119812cd0c693be461ba648540f1d7c2471
Sascha Stojanovic <sascha.stojanovic@sap.com>
2026-07-08 19:32:33 +0200

    little_help

--- Praxis/Praxis.java
+++ Praxis/Praxis.java
@@ -1,5 +1,17 @@
 package Praxis;
 
+import java.util.List;
+
 public class Praxis {
-    
+    private String myName;
+    // prv publ Datatype name
+
+    private List<Patient> myPatientenliste;
+    // prv publ Datatype name
+
+
+    public Praxis(String myName, List<Patient> name_was_missing){
+        this.myName = myName;
+        this.myPatientenliste = name_was_missing;
+    }
 }
